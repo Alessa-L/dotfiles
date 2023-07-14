7c1529b95b8b7c5f84c47bc7c508aba2180cd51b
Alessa L <alessa.code.fnrcz@slmail.me>
2023-07-14 20:38:53 -0300

    Added mingw for cross compiling

--- Documents/Linux/fedora.sh
+++ Documents/Linux/fedora.sh
@@ -83,6 +83,7 @@ sudo dnf install -y \
 # Install Rust and Cross (Tool to easily cross-compile rust code via docker)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install cross 
+sudo dnf install mingw32-gcc mingw64-gcc
 
 # Install Qtile
 sudo dnf install -y \
